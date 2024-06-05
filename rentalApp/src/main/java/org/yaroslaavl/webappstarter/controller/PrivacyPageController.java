de1f8362465458a82f4182072145395e13392f45
yaroslaavl <ylopatkin@gmail.com>
2024-06-06 01:42:50 +0200

    06.06 Finished the frontend. Keep doing Internationalization

---
+++ rentalApp/src/main/java/org/yaroslaavl/webappstarter/controller/PrivacyPageController.java
@@ -0,0 +1,17 @@
+package org.yaroslaavl.webappstarter.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Tag(name = "Privacy Page Controller")
+public class PrivacyPageController {
+
+    @Operation(summary = "Privacy policy")
+    @GetMapping("/privacy-company")
+    public String privacy(){
+        return "privacy/privacy-company";
+    }
+}
